e1f6f3577fa4d489154c4a3316f015cd626b8ce1
Menglin Yu <menglinyu@MacBook-Pro.local>
2020-02-12 15:54:28 -0500

    add Event Project

--- src/@fake-db/db/portfolios-db.js
+++ src/@fake-db/db/portfolios-db.js
@@ -1,7 +1,7 @@
 export const portfolios = [
     {
         id: 1,
-        title: 'EVENT RECOMMENDATION',
+        title: 'Event Recommendation',
         websiteUrl: 'http://18.218.234.178/Jupiter',
         githubUrl: 'https://github.com/MarilynY/Event_Project_Full_Stack.git',
         thumbnail: 'https://blog.socedo.com/wp-content/uploads/2016/09/Events.jpg',
@@ -10,7 +10,7 @@ export const portfolios = [
     },
     {
         id: 2,
-        title: 'SOCIAL AROUND',
+        title: 'Social Around',
         websiteUrl: 'https://social-around.firebaseapp.com/',
         githubUrl: 'https://github.com/MarilynY/social_around_web.git',
         //Another picture
@@ -21,7 +21,7 @@ export const portfolios = [
     },
     {
         id: 3,
-        title: 'NBA PLAYER STRENGTH VISUALIZATION',
+        title: 'NBA Player Strength Visualization',
         websiteUrl: 'http://marilyn1.com.s3-website.us-east-2.amazonaws.com/',
         githubUrl: 'https://github.com/MarilynY/nba_player_strength_visualization.git',
         thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXJ05rShm901KI3LxtL_6bwVgBOry3Ao9v_VZ10Bm8R2qo_QRx8w&s',
@@ -30,11 +30,20 @@ export const portfolios = [
     },
     {
         id: 4,
-        title: 'WORD GAME ANDROID APP',
+        title: 'Word Game Android App',
         websiteUrl: '',
         githubUrl: '',
         thumbnail: 'https://images-na.ssl-images-amazon.com/images/I/41R%2BwGB6HlL.png',
         full: 'https://www.youtube.com/watch?v=bjIZuFuUFfM&feature=youtu.be',
+        category: 'Android Studio',
+    },
+    {
+        id: 5,
+        title: 'Robot Delivery Service Web Application',
+        websiteUrl: '',
+        githubUrl: '',
+        thumbnail: 'https://images.tech.co/wp-content/uploads/2019/04/10153144/google-wing-drone-delivery.jpeg',
+        full: '',
         category: 'Android Studio',
     },
 ]
